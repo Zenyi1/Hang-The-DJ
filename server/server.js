7ca7e8cc0c31c46d75f0284bdbbcf1e288f5bbcf
Zenyi <zenyigom@hotmail.com>
2024-12-02 18:33:15 +0000

    Fix deployment configuration

--- server/server.js
+++ server/server.js
@@ -19,11 +19,17 @@ const bodyParser = require('body-parser');
 
 
 const app = express();
-app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    });
+  }
+  
 
 app.post('/webhook', bodyParser.raw({ type: 'application/json' }), webhookController.handleStripeWebhook);
 app.use(cors());
-//app.use(express.static("dist"));
+app.use(express.static("dist"));
 app.use(express.json());
 
 // Serve static files from the React app
