10bafb19ec8ea9acc496b1ea1a4947c00d1fd9a5
Zenyi <zenyigom@hotmail.com>
2025-01-31 23:14:54 +0000

    Fix deployment configuration

--- server/server.js
+++ server/server.js
@@ -201,8 +201,9 @@ app.get('*', (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
+
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
